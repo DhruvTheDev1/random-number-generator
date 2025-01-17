5cb079f8979c04ecfe0ee80fe00912a2d283314c
DhruvTheDev1 <dhruv2015@hotmail.co.uk>
2025-01-17 17:56:32 +0000

    Update RandomNumberGenerator.java
    -Testing Changes

--- src/RandomNumberGenerator.java
+++ src/RandomNumberGenerator.java
@@ -5,13 +5,13 @@ public class RandomNumberGenerator {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-
+        // min number
         System.out.print("Enter min: ");
         int min = scanner.nextInt();
-
+        // max number
         System.out.print("Enter max: ");
         int max = scanner.nextInt();
-
+        // generates number between min and max inclusive
         Random random = new Random();
         int randomNumber = random.nextInt(max + 1 - min) + min;
         System.out.println("");
